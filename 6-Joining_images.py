60838528fffd24a119cfe58b0ced32f330dfd6e7
_Phantom_ <60980662+Ashik-Phantom@users.noreply.github.com>
2020-08-22 13:11:47 +0530

    Create 6-Joining_images.py

---
+++ 6-Joining_images.py
@@ -0,0 +1,16 @@
+# Joining images
+
+import cv2
+import numpy as np
+
+img = cv2.imread('Resources/charizard.png')
+
+imghor = np.hstack((img, img))   # Horizontal Join
+imgver = np.vstack((img, img))   # Vertical Join
+
+cv2.imshow("horizontal", imghor)
+cv2.imshow("vertical", imgver)
+
+cv2.waitKey(0)
+
+#Note: pictures with different filters cannet be merged , we have to use seperate funtion to merge them which will be available in code 6.1
