0293c1f4023ab8fed8cba80dc90ff2ee62dde553
_Phantom_ <60980662+Ashik-Phantom@users.noreply.github.com>
2020-08-22 12:52:21 +0530

    Read_and_show_a_picture

---
+++ read_and_show_picture.py
@@ -0,0 +1,9 @@
+import cv2 
+
+img=cv2.imread("Resources/charizard.png")
+
+cv2.imshow("Charizard",img)
+
+cv2.waitKey(0) 
+
+#This programs gets picture from the path mentioned and prints the picture
